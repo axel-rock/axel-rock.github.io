77aab3fe752c48a9855136586725422cd039df5e
axel-rock <rockaxel@gmail.com>
2023-08-28 23:24:08 +0200

    Clean code, remove Firebase related elements

--- .firebase/axel-rock/functions/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/projects/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.33e83772.js","_app/immutable/chunks/scheduler.b0fd5642.js","_app/immutable/chunks/index.321044db.js"];
-export const stylesheets = ["_app/immutable/assets/3.270b94ee.css"];
-export const fonts = [];
